89857082a6b0aed22831c831e9918169e60ba5ce
kovid <kovidsaivemuri@gmail.com>
2024-09-16 15:02:04 +0530

    2 done

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -17,15 +17,15 @@ export class DataManipulator {
          const priceDEF = (serverRespond [1].top_ask.price + serverRespond [1].top_bid.price) / 2;
          const ratio = priceABC / priceDEF;
          const upperBound = 1 + 0.05;
-         const lowerBound = 1 0.05;
+         const lowerBound = 1- 0.05;
          return {
              price_abc: priceABC,
              price_def: priceDEF,
              ratio,
              timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
                 serverRespond[0].timestamp: serverRespond [1].timestamp,
-             upper bound: upperBound,
-             lower bound: lowerBound,
+             upper_bound: upperBound,
+             lower_bound: lowerBound,
              trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio: undefined,
          };
      }
